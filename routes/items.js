7f0a4247dbcbe30a49ac025f17acedb61761642e
nonso <chinonsojacobs@gmail.com>
2021-06-25 20:58:59 +0100

    initial commit

--- routes/items.js
+++ routes/items.js
@@ -1,17 +1,90 @@
-const items = require('../items')
+const {getItems, getItem, addItem, deleteItem, updateItem} = require('../controllers/itemcontroller')
+
+const Item = {
+    type: 'object',
+    properties: {
+        id: {type: 'string'},
+        name: {type: 'string'}
+    }
+}
+//Options for get all items
+const getItemsOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                items: Item
+            }
+        }
+    },
+    handler: getItems
+}
+//Options for get single item
+const getItemOptions = {
+    schema: {
+        response: {
+            200: Item
+        }
+    },
+    handler: getItem
+}
+//Options for add item
+const postItemOptions = {
+    schema: {
+        body: {
+            type: 'object',
+            required: ['name'],
+            properties: {
+                name: {type: 'string'}
+            }
+        },
+        response: {
+            201: Item
+        }
+    },
+    handler: addItem
+}
+//Options for delete item
+const deleteItemOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    message: {type: 'string'}
+                }
+            }
+        }
+    },
+    handler: deleteItem
+}
+//Options for update item
+const updateItemOptions = {
+    schema: {
+        response: {
+            200: Item
+        }
+    },
+    handler: updateItem
+}
 
 async function itemRoutes (fastify, options, done) {
-    fastify.get('/items', async function(request, reply) {
-        reply.send(items)
-    })
+    // Get all items
+    fastify.get('/items', getItemsOptions)
 
-    fastify.get('/items/:id', async function(request, reply) {
-        const {id} = request.params
+    //Get single item
+    fastify.get('/items/:id', getItemOptions)
 
-        const item = items.find((item) => item.id === id)
+    //Add item
+    fastify.post('/items', postItemOptions)
 
-        reply.send(item)
-    })
+    //Delete item
+    fastify.delete('/items/:id', deleteItemOptions)
+
+    //Update item
+    fastify.put('/items/:id', updateItemOptions)
+    
+    done()
 }
 
 module.exports = itemRoutes
--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const fastify = require('fastify')({
     logger: null
 })
 
+fastify.register(require('fastify-swagger'), {
+    exposeRoute: true,
+    routePrefix: '/docs',
+    swagger: {
+        info: {
+            title: 'fastify-api'
+        }
+    }
+})
 fastify.register(require('./routes/items'))
 
 const start = async() => {
